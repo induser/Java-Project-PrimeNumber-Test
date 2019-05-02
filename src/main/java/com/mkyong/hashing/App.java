b5073c7bb3e358aff67858b939d50680ccaf1be4
Anil <anilkumart2020@gmail.com>
2019-05-02 04:09:11 -0700

    commit

---
+++ src/main/java/com/mkyong/hashing/App.java
@@ -0,0 +1,23 @@
+package com.mkyong.hashing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+public boolean isprime(int n){
+boolean val =true;
+for (int i = 2; i<=n/2; i++) {
+  if (n%i ==0) {
+     val = false;
+     break;
+  }
+}
+return val;
+}
+}
---
+++ src/test/java/com/mkyong/hashing/AppTest.java
@@ -0,0 +1,46 @@
+package com.mkyong.hashing;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+    public void testprime1() {
+       boolean f1 = new App().isprime(5);
+        assertTrue( f1 );  
+    }
+    public void testprime11() {
+       boolean f11 = new App().isprime(8);
+        assertFalse( f11);  
+    }
+}
